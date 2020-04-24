84a50b1656ec04d3fb098644b6b89071a0230476
pklimesh <63873212+pklimesh@users.noreply.github.com>
2020-04-24 15:34:06 -0500

    base.CalculateStatistics();

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,16 +10,6 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
-        public virtual void CalculateStatistics(string name)
-        {
-            if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-        }
-        public virtual void CalculateStudentStatistics(string name)
-        {
-            if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-        }
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
@@ -38,6 +28,24 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+                base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
