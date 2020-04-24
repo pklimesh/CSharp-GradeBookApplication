025a83c8b3449b58c74c70f61e34484072dfecf6
pklimesh <63873212+pklimesh@users.noreply.github.com>
2020-04-24 14:23:23 -0500

    C#-7 Create GreadeBookType

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,15 +10,21 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
-         public virtual char GetLetterGrade(double averageGrade)
+        public override char GetLetterGrade(double averageGrade)
         {
-            if (averageGrade >= 80)
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-            else if (averageGrade >= 60)
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
                 return 'B';
-            else if (averageGrade >= 40)
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
                 return 'C';
-            else if (averageGrade >= 20)
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
             else
                 return 'F';
